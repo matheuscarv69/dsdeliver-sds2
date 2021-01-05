f7dfab4bd50b4090b4650ec04b873e12826bd994
matheuscarv69 <matheus9126@gmail.com>
2021-01-05 09:50:27 -0400

    GET /orders

--- backend/src/main/java/services/OrderService.java
+++ backend/src/main/java/services/OrderService.java
@@ -1,9 +1,25 @@
 package services;
 
+import dto.OrderDTO;
+import entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import repositories.OrderRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
 
+    @Autowired
+    private OrderRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<OrderDTO> findAll() {
+        List<Order> list = repository.findOrdersWithProducts();
+        return list.stream().map(p -> new OrderDTO(p)).collect(Collectors.toList());
+    }
 
 }
