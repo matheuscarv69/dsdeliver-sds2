1464a65937e734f73e8f191ce9c09930491d9239
matheuscarv69 <matheus9126@gmail.com>
2021-01-05 10:09:16 -0400

    POST /orders

--- backend/src/main/java/controllers/OrderController.java
+++ backend/src/main/java/controllers/OrderController.java
@@ -3,11 +3,11 @@ package controllers;
 import dto.OrderDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import services.OrderService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +21,15 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll() {
         List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
--- backend/src/main/java/services/OrderService.java
+++ backend/src/main/java/services/OrderService.java
@@ -1,12 +1,17 @@
 package services;
 
 import dto.OrderDTO;
+import dto.ProductDTO;
 import entities.Order;
+import entities.OrderStatus;
+import entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import repositories.OrderRepository;
+import repositories.ProductRepository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +21,28 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrdersWithProducts();
         return list.stream().map(p -> new OrderDTO(p)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+
+        for (ProductDTO p : dto.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+
+        order = repository.save(order);
+
+        return new OrderDTO(order);
     }
 
 }
