e43c6d58836a2845f9dd8961ab74b0248a75df35
matheuscarv69 <matheus9126@gmail.com>
2021-01-05 08:53:41 -0400

    Domain model

--- backend/src/main/java/config/SecurityConfig.java
+++ backend/src/main/java/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
