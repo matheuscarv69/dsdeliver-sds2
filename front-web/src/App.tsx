f2aed010eddddc8ab406cc88b3b0d80318dc98ab
matheuscarv69 <matheus9126@gmail.com>
2021-01-08 20:03:30 -0400

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import "./App.css";
-import Navbar from "./Navbar";
 import Routes from "./Routes";
 
 function App() {
   return (
-   <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
