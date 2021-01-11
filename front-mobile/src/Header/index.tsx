4babdb2a131f2f072f00ce3eee6211b1f443ac15
matheuscarv69 <matheus9126@gmail.com>
2021-01-10 21:18:47 -0400

    Add order list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate("Home");
+  };
+
   return (
-    <View style={styles.container}>
-      <Image source={require("../assets/logo.png")} />
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require("../assets/logo.png")} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -25,6 +35,6 @@ const styles = StyleSheet.create({
     letterSpacing: -0.24,
     color: "#fff",
     marginLeft: 13,
-    fontFamily: 'OpenSans_700Bold'
+    fontFamily: "OpenSans_700Bold",
   },
 });
